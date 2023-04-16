c6bdcbe11229e75d00664c794dc608fc4caed41d
nosianuri <nurinosia@gmail.com>
2023-04-17 03:17:13 +0600

    update litigation content

--- src/components/Litigation/Litigation.js
+++ src/components/Litigation/Litigation.js
@@ -7,64 +7,64 @@ const Litigation = () => {
       url: 'https://i.ibb.co/2W415ds/Roundup-1.jpg',
       link: '/roundup',
       title: 'Roundup',
-      subTitle: 'FDA calls for Zantac to be pulled from market immediately',
-      desc: 'Monsanto has settled over 100,000 Roundup lawsuits, paying out about $11 billion as of May 2022. The recent Roundup settlement will pay as much as $9.6 billion to resolve victims involved in current litigation and set aside $1.25 billion to pay future claims as part of separate class-action lawsuits. The average Roundup lawsuit payout for Round One settlements was in the $120,000 to $180,000 per person range.Monsanto, a well-known agricultural company, has been at the center of controversy over its Roundup weed killer for years. In recent times, the company has faced a flood of lawsuits from individuals claiming that the product caused them to develop cancer...',
+      subTitle: 'Monsanto has settled over 100,000 Roundup lawsuits of May 2022',
+      desc: 'Monsanto has settled over 100,000 Roundup lawsuits, paying out about $11 billion as of May 2022',
     },
     {
       id: 2,
       url: 'https://i.ibb.co/rmqppBZ/camp.png',
       link: '/camplejeune',
       title: 'Camp Lejeune',
-      subTitle: 'FDA calls for Zantac to be pulled from market immediately',
-      desc: 'Camp Lejeune lawsuit settlement payouts for contaminated water exposure is estimated to be over $6.7 billion. Victims who pursue a Camp lejeune toxic water claim will receive lucrative settlements which could be well over $1 million in some cases. However, another renowned law firm stated that settlement payouts for Camp Lejeune toxic exposure victims are estimated at $6.7 billion, individuals who file a claim will receive between $25,000 and $1 million or more, depending on the severity of their illness.The United States Marine Corps Camp Lejeune has long been the subject of controversy over its contaminated water supply, which led to numerous health problems for those stationed there. ..',
+      subTitle: 'Camp Lejeune lawsuit payouts for contaminated water exposure',
+      desc: 'Camp Lejeune lawsuit settlement payouts for contaminated water exposure is estimated',
     },
     {
       id: 3,
       url: 'https://i.ibb.co/hB2yH6n/car-mva.png',
       link: '/mva',
       title: 'MVA',
-      subTitle: 'FDA calls for Zantac to be pulled from market immediately',
-      desc: 'The Insurance Information Institute reports that the typical car accident settlement amount for bodily injury was $15,785 and the average compensation for property damage was $3,841 in 20181. Forbes also reports that the median amount awarded in auto accident cases was $16,000.Car accidents are unfortunately all too common on our roads, and the aftermath can be both physically and financially devastating. If you have been involved in a car accident, you may be wondering what kind of settlement you can expect to receive.However, its important to note that every case is unique, and settlement amounts can vary widely depending on the circumstances of the accident...',
+      subTitle: 'Car accidents are unfortunately all too common on our roads',
+      desc: 'The Insurance Information Institute reports that the typical car accident settlement',
     },
     {
       id: 4,
       url: 'https://i.ibb.co/cDWBrW5/talcum.png',
       link: '/talcome',
       title: 'Talcum Powder',
-      subTitle: 'FDA calls for Zantac to be pulled from market immediately',
-      desc: 'Johnson & Johnson paid $100,000,000 to settle a group of 1,000 talcum powder lawsuits.Johnson & Johnson announced that it will pay $8.9 billion to settle talcum powder lawsuits. The average compensation payout for talc powder victims is $4.4 million. Legal-Bay LLC reports that Johnson & Johnson will pay out $8.9 billion in damages to settle the talcum powder lawsuit.The use of talcum powder has been a common practice for many years, but recent lawsuits have shed light on the potential dangers of this product. Johnson & Johnson, one of the leading producers of talcum powder, has faced numerous lawsuits from individuals claiming that the product caused them to develop cancer... ',
+      subTitle: 'Johnson & Johnson to settle a group of 1,000 talcum powder lawsuits',
+      desc: 'Johnson & Johnson paid $100,000,000 to settle a group of 1,000 talcum powder lawsuits',
     },
     {
       id: 5,
       url: 'https://i.ibb.co/G5n0s1t/nec-baby.png',
       link: '/nec',
       title: 'NEC',
-      subTitle: 'FDA calls for Zantac to be pulled from market immediately',
-      desc: 'The average verdict or settlement payout in a sampling of recent malpractice lawsuits for NEC in premature infants was around $3.5 million, but some cases may be worth more or less than that. The estimated settlement amounts for NEC lawsuits may range between $5,000 to over $500,000, but these are not guaranteed and depend on the outcome of each case. The compensation amount for NEC lawsuits may vary depending on the severity of the injury, the fault of the defendant, the future medical expenses, and other factors...',
+      subTitle: 'NEC lawsuits may vary depending on the severity of the injury',
+      desc: 'The average verdict or settlement payout in a sampling of recent malpractice lawsuits',
     },
     {
       id: 6,
       url: 'https://i.ibb.co/3MywvNN/tylenol-service.png',
       link: '/tylenol',
       title: 'Tylenol',
-      subTitle: 'FDA calls for Zantac to be pulled from market immediately',
-      desc: 'The average verdict or settlement payout in a sampling of recent malpractice lawsuits for NEC in premature infants was around $3.5 million, but some cases may be worth more or less than that. The estimated settlement amounts for NEC lawsuits may range between $5,000 to over $500,000, but these are not guaranteed and depend on the outcome of each case. The compensation amount for NEC lawsuits may vary depending on the severity of the injury, the fault of the defendant, the future medical expenses, and other factors...',
+      subTitle: 'A Tylenol autism class action lawsuit has been certified and estimated',
+      desc: 'The lawsuit was filed on behalf of thousands of families across the United States who claim',
     },
     {
       id: 7,
       url: 'https://i.ibb.co/d5CBS1d/fire.png',
       link: '/fire-fighter-foam',
       title: 'Fire Fighter Foam',
-      subTitle: 'FDA calls for Zantac to be pulled from market immediately',
-      desc: 'The average verdict or settlement payout in a sampling of recent malpractice lawsuits for NEC in premature infants was around $3.5 million, but some cases may be worth more or less than that. The estimated settlement amounts for NEC lawsuits may range between $5,000 to over $500,000, but these are not guaranteed and depend on the outcome of each case. The compensation amount for NEC lawsuits may vary depending on the severity of the injury, the fault of the defendant, the future medical expenses, and other factors...',
+      subTitle: 'Firefighting foam or (AFFF) has been used to extinguish fires',
+      desc: 'AFFF firefighting foam, which contains toxic PFAS chemicals, has been linked to a range of',
     },
     {
       id: 8,
       url: 'https://i.ibb.co/Qn5182C/hair-relaxer.jpg',
       link: '/hair-relaxer',
       title: 'Hair Relaxer',
-      subTitle: 'FDA calls for Zantac to be pulled from market immediately',
-      desc: 'The average verdict or settlement payout in a sampling of recent malpractice lawsuits for NEC in premature infants was around $3.5 million, but some cases may be worth more or less than that. The estimated settlement amounts for NEC lawsuits may range between $5,000 to over $500,000, but these are not guaranteed and depend on the outcome of each case. The compensation amount for NEC lawsuits may vary depending on the severity of the injury, the fault of the defendant, the future medical expenses, and other factors...',
+      subTitle: 'The first hair relaxer lawsuit was filed in 2011',
+      desc: 'In a hair relaxer lawsuit, the compensation amount you receive will depend on various',
     },
   ];
 
@@ -82,7 +82,7 @@ const Litigation = () => {
               <div>
                 <h3 className='text-[#13100E]  text-lg'><a className='hover:text-[#800020]' href="/zantac">{blog.title}</a></h3>
                 <h5 className='text-[#300303] text-md leading-none mb-1 overflow-y-hidden'>{blog.subTitle}</h5>
-                <p className='text-xs '>The FDA has warned that Zantac® may be contaminated with cancer-causing agent...<span className='text-[#800020] font-bold'><a className='hover:text-[#13100E]' href={blog.link}> MORE »</a></span></p>
+                <p className='text-xs '>{blog.desc}...<span className='text-[#800020] font-bold'><a className='hover:text-[#13100E]' href={blog.link}> MORE »</a></span></p>
               </div>
             </div>
           </div>
