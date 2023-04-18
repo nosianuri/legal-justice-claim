b588432d06de1a6c405748156447dca268add078
nosianuri <nurinosia@gmail.com>
2023-04-18 23:04:41 +0600

    update responsive menu

--- src/components/LegalHeader/HeaderMenu.js
+++ src/components/LegalHeader/HeaderMenu.js
@@ -7,29 +7,77 @@ const HeaderMenu = () => {
     const [searchlaw, setSearchlaw] = useState("");
     const [showDropdown, setShowDropdown] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
-    const sectionRef = useRef(null);
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-          if (sectionRef.current && !sectionRef.current.contains(event.target)) {
-            setIsOpen(false);
-          }
-        };
-    
-        document.addEventListener('click', handleClickOutside);
-    
-        return () => {
-          document.removeEventListener('click', handleClickOutside);
-        };
-      }, [sectionRef]);
+    const [open, setOpen] = useState(false);
+    const toggleShowMore = () => setOpen(!open);
+    const toggleShowMenu = () => setIsOpen(!isOpen);
+    // const sectionRef = useRef(null);
+    const menus = [
+        {
+            id: 1,
+            name: 'Camp Lejeune',
+            Link: '/camplejeune',
+        },
+        {
+            id: 2,
+            name: 'NEC',
+            Link: '/nec',
+        },
+        {
+            id: 3,
+            name: 'RoundUp',
+            Link: '/roundup',
+        },
+        {
+            id: 4,
+            name: 'MVA',
+            Link: '/mva',
+        },
+        {
+            id: 5,
+            name: 'Talcum Powder',
+            Link: '/talcome',
+        },
+        {
+            id: 6,
+            name: 'Tylenol',
+            Link: '/tylenol',
+        },
+        {
+            id: 7,
+            name: 'Hair Relaxer',
+            Link: '/hair-relaxer',
+        },
+        {
+            id: 8,
+            name: 'Fire Fighter Foam',
+            Link: '/fire-fighter-foam',
+        },
+    ];
+    // useEffect(() => {
+    //     const handleClickOutside = (event) => {
+    //         if (sectionRef.current && !sectionRef.current.contains(event.target)) {
+    //             setIsOpen(false);
+    //         }
+    //     };
 
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
+    //     document.addEventListener('click', handleClickOutside);
+
+    //     return () => {
+    //         document.removeEventListener('click', handleClickOutside);
+    //     };
+    // }, [sectionRef]);
+
+    // const handleClick = () => {
+    //     setIsOpen(!isOpen);
+    // };
+    // const clickLawsuits = () => {
+    //     setOpen(!isOpen);
+    // };
 
     const handleDropdown = () => {
         setShowDropdown(!showDropdown);
     };
-   
+
 
     const onSearch = (searchTerm) => {
         setSearchlaw(searchTerm);
@@ -79,18 +127,24 @@ const HeaderMenu = () => {
                 </div>
             </div>
             <div className='block sm:hidden'>
-                <div  className='text-[16px] px-8 py-2 border border-[#131416] border-r-gray-400 font-semibold navigation__item hover:bg-[#333] hover:bg-opacity-30' ref={sectionRef} onClick={handleClick}>Menu <i class="fa-solid fa-angle-down pl-1"></i>
+                <div className='text-[16px] px-8 py-2 border border-[#131416] border-r-gray-400 font-semibold navigation__item hover:bg-[#333] hover:bg-opacity-30'  onClick={toggleShowMenu}>{isOpen ? 'Menu' : 'Menu'} <i class="fa-solid fa-angle-down pl-1"></i>
                     {isOpen && (
-                        <ul className="dropdown_nav mt-2 fixed z-50">
+                        <ul className="dropdown_nav mt-2 absolute z-50">
                             <li className="dropdown__item py-1 pl-4 border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/">Home</a></li>
-                            <li className="dropdown__item py-1 pl-4 border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/roundup">Roundup</a></li>
-                            <li className="dropdown__item py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/camplejeune">Camp Lejeune </a></li>
-                            <li className="dropdown__item py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/mva">MVA</a></li>
-                            <li className="dropdown__item py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/talcome">Talcome Powder</a></li>
-                            <li className="dropdown__item py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/nec">NEC</a></li>
-                            <li className="dropdown__item py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/tylenol">Tylenol</a></li>
-                            <li className="dropdown__item py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/fire-fighter-foam">Fire Fighter Foam</a></li>
-                            <li className="dropdown__item py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/hair-relaxer">Hair Relaxer</a></li>
+                            <li className="dropdown__item py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"  onClick={toggleShowMore}> {open ? 'Lawsuits' : 'Lawsuits'}<i class="fa-solid fa-angle-down pl-1"></i>
+                                {open && (
+                                    <ul className=" mt-2 ">
+                                        <li className=" py-1 pl-4 border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/roundup">Roundup</a></li>
+                                        <li className=" py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/camplejeune">Camp Lejeune </a></li>
+                                        <li className=" py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/mva">MVA</a></li>
+                                        <li className=" py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/talcome">Talcome Powder</a></li>
+                                        <li className=" py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/nec">NEC</a></li>
+                                        <li className=" py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/tylenol">Tylenol</a></li>
+                                        <li className=" py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/fire-fighter-foam">Fire Fighter Foam</a></li>
+                                        <li className=" py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/hair-relaxer">Hair Relaxer</a></li>
+                                    </ul>
+                                )}
+                            </li>
                             <li className="dropdown__item py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/legal-news">Legal News</a></li>
                             <li className="dropdown__item py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/testimonial">Testimonial</a></li>
                             <li className="dropdown__item py-1 pl-4  border border-[#cf2e2e] border-b-[#be2929] hover:bg-[#cc4545]"><a href="/about-us">About Us</a></li>
